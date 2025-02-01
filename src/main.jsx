0f5e9da6872d50b4c3f47e40626c80c50fb4a5d0
Wilmer Guzman <joelguzman2468@gmail.com>
2025-02-01 03:02:09 -0500

    Setup GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  HashRouter,
+} from "react-router";
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import { ThemeProvider } from "./Context/ThemeProvider.jsx";
@@ -9,18 +15,18 @@ import Details from "./pages/Details.jsx";
 import { FiltersProvider } from "./Context/FiltersProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <ThemeProvider>
         <FiltersProvider>
           <Navbar />
           <Routes>
-            <Route index path="/" element={<Navigate to={"/home"} replace/>}/> 
+            <Route index path="/" element={<Navigate to={"/home"} replace />} />
             <Route path="/home" element={<Home />} />
             <Route path="/country/:name" element={<Details />} />
           </Routes>
         </FiltersProvider>
       </ThemeProvider>
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
